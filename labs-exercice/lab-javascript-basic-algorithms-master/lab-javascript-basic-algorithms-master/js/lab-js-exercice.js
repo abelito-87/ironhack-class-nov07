33ebe148e0f8e0d1e214837e1c677e7d36c54406
Abel Manjon <abel.manjon@gmail.com>
2022-11-18 17:03:18 +0100

    lab-promise.js

--- labs-exercice/lab-javascript-basic-algorithms-master/lab-javascript-basic-algorithms-master/js/lab-js-exercice.js
+++ labs-exercice/lab-javascript-basic-algorithms-master/lab-javascript-basic-algorithms-master/js/lab-js-exercice.js
@@ -1,10 +1,10 @@
-                    // Iteration 1: Names and Input
-//
+ // Iteration 1: Names and Input
+
 
 let hacker1 = "Ana Isabel";
 console.log(`The driver's name is ${hacker1}`);
 let hacker2 = "Kase O"
-console.log(`The navigator's name is ${hacker2}`); // ${} plantillas 
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
@@ -33,6 +33,12 @@ for (let i = hacker1.length -1; i >= 0; i--) {
 }
 console.log(reverseName);
 
-// Iteracion Usando metodo nativo
-// const reverseNameSimplified = hacker1.split("").reverse().join("");
-// console.log(reverseNameSimplified);
+
+//BONUS TIME 1 
+
+
+const lorem = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Quis ad voluptatum beatae quisquam autem nisi ullam nihil a veniam nam pariatur dolores temporibus animi atque,  quidem sapiente veritatis quae adipi etLorem ipsum dolor, sit amet consectetur adipisicing elit.Quis ad voluptatum beatae quisquam autem nisi ullam nihil a veniam nam pariatur dolores temporibus animi atque, quidem sapiente veritatis quae adipi et Lorem ipsum dolor, sit amet consectetur adipisicing elit.Quis ad voluptatum beatae quisquam autem nisi ullam nihil a veniam nam pariatur dolores temporibus animi atque, quidem sapiente veritatis quae adipi et";
+
+for (let key in lorem) {
+    console.log("et");
+  }
