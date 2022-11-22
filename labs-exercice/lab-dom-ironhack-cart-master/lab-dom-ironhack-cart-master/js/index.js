548e7c2722be0efbae827e834f295f6db544e4a5
Abel Manjon <abel.manjon@gmail.com>
2022-11-22 19:55:04 +0100

    lab-ironcart

--- labs-exercice/lab-dom-ironhack-cart-master/lab-dom-ironhack-cart-master/js/index.js
+++ labs-exercice/lab-dom-ironhack-cart-master/lab-dom-ironhack-cart-master/js/index.js
@@ -2,12 +2,12 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
- const price = product.querySelector(".price span");
- const quantity= product.querySelector(".quantity input");;
-console.log(price, quantity);
+  const price = product.querySelector(".price span");
+  const quantity= product.querySelector(".quantity input");;
+  console.log(price, quantity);
  //traer elementos dom
- const priceValue = parseFloat(price.innerText);
- const quantityValue = quantity.valueNumber;
+  const priceValue = parseFloat(price.innerText);
+  const quantityValue = quantity.valueAsNumber;
  //extraer valores
   //... your code goes here
   const subTotalValue = priceValue * quantityValue;
@@ -15,20 +15,28 @@ console.log(price, quantity);
   //obtener elementos del dom === subtotal
   subTotal.innerText = subTotalValue;
  // JA CAGADA, JA NO SURT :(
+  return subTotalValue;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+ // updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  // ITERATION 2^
+  let sumTotal = 0;
+  const products = document.getElementsByClassName('product');
+  for(let i = 0; i < products.length; i++){
+    sumTotal += updateSubtotal(products[i]);
+  }
+  
+  
 
   // ITERATION 3
   //... your code goes here
+  document.querySelector("#total-value span").innerText = sumTotal;
 }
 
 // ITERATION 4
