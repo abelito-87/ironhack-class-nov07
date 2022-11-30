077896eb30fdd5973b5a7143ae7b3710651a38d6
Abel Manjon <abel.manjon@gmail.com>
2022-11-30 08:41:36 +0100

    lab-movies-30-11

--- labs-exercice/lab-javascript-greatest-movies-master/lab-javascript-greatest-movies-master/src/movies.js
+++ labs-exercice/lab-javascript-greatest-movies-master/lab-javascript-greatest-movies-master/src/movies.js
@@ -2,19 +2,67 @@
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
+
 function getAllDirectors(moviesArray) {
- 
+ const arrayDirectors = [];
+ moviesArray.forEach(element => {
+    arrayDirectors.push(element.director); 
+ });
+ return arrayDirectors;
 };
-console.log(todosLosDirectores(movies));
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+ let num = 0;
+ if(moviesArray.length === 0) {
+    num = 0;
+ } else {
+    let aux = moviesArray.filter(element => element.director === 'Steven Spielberg');
+    if(aux.length === 0){
+        num = 0;
+    }else{
+        aux.forEach(e => {
+            if(e.genre.includes('Drama')){
+                num++;
+            }
+        })
+    }
+ }
+ return num;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+   let scoreArray = 0;
+    moviesArray.forEach(element => {
+        if(element.score){
+            scoreArray += element.score; 
+        }
+    });
+    if(scoreArray > 0){
+       return parseFloat((scoreArray / moviesArray.length).toFixed(2));
+    }else{
+        return 0;
+    }
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    let dramaScore = 0;
+    let dramaCount = 0;
+    moviesArray.forEach(element => {
+        if(element.genre.includes("Drama")){
+            dramaCount++;
+            dramaScore += element.score;
+        }
+    });
+    if(dramaCount ==0){
+        return 0;
+    }else{
+        return parseFloat((dramaScore / dramaCount).toFixed(2));
+    }
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
